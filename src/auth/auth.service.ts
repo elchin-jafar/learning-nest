e9a66781ed161296c531cfa99e3b1c2e4c291ad6
elchin <elcinceferli@gmail.com>
2025-12-28 04:03:14 +0400

    initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+    signin() {
+        return {
+            msg: 'I have signin in'
+        }
+    }
+
+    signup() {
+        return {
+            msg: 'I have signed up'
+        }
+    }
+}
